a3ace7f026625a6eb8e431bbfb49d1f4264c0e1b
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-04-04 00:50:19 -0300

    final update

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
   return (
@@ -15,7 +16,7 @@ const GetApp = () => {
           <p className="regular-16 text-gray-10">
             Available on IOS and Android
           </p>
-          <div>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
             <Button
               type="button"
               title="Download App"
@@ -31,6 +32,10 @@ const GetApp = () => {
               full
             />
           </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phones" width={550} height={870} />
         </div>
       </div>
     </section>
