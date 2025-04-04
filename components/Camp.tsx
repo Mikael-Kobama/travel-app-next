3ae88ade08a74ee2a0c10de375655ca83d071d20
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-04-03 23:48:10 -0300

    update features

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -82,7 +82,7 @@ const Camp = () => {
   };
 
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 select-none">
       <div
         ref={containerRef}
         className="flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto scroll-smooth lg:h-[400px] xl:h-[640px]"
